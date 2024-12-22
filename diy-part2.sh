c8a5956795a7c84edde6fdd6acc023000193bd82
LHSFK <w2821903527@vip.qq.com>
2024-12-22 17:51:07 +0800

    Fix 5.15

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,21 +14,21 @@
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-sed -i 's/LEDE/OpenWrt/g' package/base-files/files/bin/config_generate
-sed -i 's/LEDE/OpenWrt/g' package/base-files/files/etc/init.d/system
+sed -i 's/LEDE/iStoreOS/g' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/iStoreOS/g' package/base-files/files/etc/init.d/system
 sed -i 's/LEDE/OpenWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-sed -i 's/LEDE/OpenWrt/g' package/base-files/luci2/bin/config_generate
+sed -i 's/LEDE/iStoreOS/g' package/base-files/luci2/bin/config_generate
 sed -i 's/LEDE/iStoreOS/g' package/lean/default-settings/files/zzz-default-settings
 
 
-echo '                                                        
-   [0;1;34;94m▀[0m     [0;34m▄▄▄▄[0m    [0;34m▄[0m                          [0;1;30;90m▄▄▄▄[0m   [0;1;30;90m▄▄▄▄[0m 
- [0;34m▄▄▄[0m    [0;34m█▀[0m   [0;34m▀[0m [0;34m▄[0;37m▄█▄▄[0m   [0;37m▄▄▄[0m    [0;37m▄[0m [0;1;30;90m▄▄[0m   [0;1;30;90m▄▄▄[0m   [0;1;30;90m▄▀[0m  [0;1;30;90m▀[0;1;34;94m▄[0m [0;1;34;94m█▀[0m   [0;1;34;94m▀[0m
-   [0;34m█[0m    [0;37m▀█▄▄▄[0m    [0;37m█[0m    [0;37m█▀[0m [0;1;30;90m▀█[0m   [0;1;30;90m█▀[0m  [0;1;30;90m▀[0m [0;1;30;90m█▀[0m  [0;1;34;94m█[0m  [0;1;34;94m█[0m    [0;1;34;94m█[0m [0;1;34;94m▀█▄▄▄[0m 
-   [0;37m█[0m        [0;37m▀█[0m   [0;1;30;90m█[0m    [0;1;30;90m█[0m   [0;1;30;90m█[0m   [0;1;30;90m█[0m     [0;1;34;94m█▀▀▀▀[0m  [0;1;34;94m█[0m    [0;34m█[0m     [0;34m▀█[0m
- [0;37m▄▄█▄▄[0m  [0;1;30;90m▀▄▄▄█▀[0m   [0;1;30;90m▀▄▄[0m  [0;1;30;90m▀█[0;1;34;94m▄█▀[0m   [0;1;34;94m█[0m     [0;1;34;94m▀█▄▄[0;34m▀[0m   [0;34m█▄▄█[0m  [0;34m▀▄▄▄█▀[0m
-                                                        
-                                  Powered by OpenWRT    
+echo '
+   ▀     ▄▄▄▄    ▄                          ▄▄▄▄   ▄▄▄▄
+ ▄▄▄    █▀   ▀ ▄▄█▄▄   ▄▄▄    ▄ ▄▄   ▄▄▄   ▄▀  ▀▄ █▀   ▀
+   █    ▀█▄▄▄    █    █▀ ▀█   █▀  ▀ █▀  █  █    █ ▀█▄▄▄
+   █        ▀█   █    █   █   █     █▀▀▀▀  █    █     ▀█
+ ▄▄█▄▄  ▀▄▄▄█▀   ▀▄▄  ▀█▄█▀   █     ▀█▄▄▀   █▄▄█  ▀▄▄▄█▀
+
+                                  Powered by OpenWRT
  -------------------------------------------------------
  %D %V, %C
  -------------------------------------------------------' >  package/base-files/files/etc/banner
