fc8300c4802c526422ea73f7ddbfa4008bffe7df
LHSFK <w2821903527@vip.qq.com>
2025-02-15 00:27:00 +0800

    Msg: Update Cudy_TR3000 Config ; Time: 2025/02/15 00:25 CST

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,3 @@
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
-echo 'src-git fros https://github.com/LHSFK/fros-packages-immortalwrt-mt798x.git;fros-21.02' >> feeds.conf.default
